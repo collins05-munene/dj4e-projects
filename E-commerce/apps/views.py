0e00cd9a697eb2ca15468477c129416679719508
munenecollins <collinsgitonga296@gmail.com>
2026-03-20 09:15:09 +0300

    Created the homepage

--- E-commerce/apps/views.py
+++ E-commerce/apps/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Item, Client, Admin
 
 # Create your views here.
+class Homepage(View):
+    def get(self, request):
+        items = Item.objects.all()
+        context = {'items': items}
+        return render(request, 'apps/homepage.html', context)
+
