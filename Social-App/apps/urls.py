c5169081a1b5923981db23922d8640671ae5d061
munenecollins <collinsgitonga296@gmail.com>
2026-03-26 09:51:09 +0300

    Completed Login and Regsitrations features

--- Social-App/apps/urls.py
+++ Social-App/apps/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.Homepage.as_view(), name='homepage'),
     path('create-account', views.CustomRegistrationView.as_view(), name='create-user'),
-    
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('client-page', views.ClientPage.as_view(), name='client-page'),
+
 ]
