800323d1d81e2e284feadf1f27e127f40a9c7562
munenecollins <collinsgitonga296@gmail.com>
2026-03-27 08:56:01 +0300

    Worked on the update client details

--- Social-App/apps/urls.py
+++ Social-App/apps/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.Homepage.as_view(), name='homepage'),
-    path('create-account', views.CustomRegistrationView.as_view(), name='create-user'),
+    path('create/account', views.CustomRegistrationView.as_view(), name='create-user'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('client-page', views.ClientPage.as_view(), name='client-page'),
-    path('account/delete/', views.ClientDeleteView.as_view(), name='delete-account')
-
+    path('client/page', views.ClientPage.as_view(), name='client-page'),
+    path('account/delete/', views.ClientDeleteView.as_view(), name='delete-account'),
+    path('update/client/', views.ClientUpdateView.as_view(), name='update-client'),
+  
 ]
